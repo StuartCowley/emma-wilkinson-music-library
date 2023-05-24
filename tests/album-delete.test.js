ece01c9432ec4a9e965c86e2ac73aafb6b360be7
Emma Wilkinson <emmawilkinsonnxo@gmail.com>
2023-05-24 14:33:55 +0100

    all my tests pass

--- tests/album-delete.test.js
+++ tests/album-delete.test.js
@@ -4,35 +4,42 @@ const db = require('../src/db')
 const app = require('../src/app')
 
 describe('Delete Album', () => {
+  let album;
   let artist;
   beforeEach(async () => {
-    const { rows } = await db.query('INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *', [
+    const artistTable = await db.query('INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *', [
       'MF DOOM',
       'rap',
     ]);
-    artist = rows[0];
-  });
-  let album
-  beforeEach(async () => {
-    const { rows } = await db.query('INSERT INTO Albums (name, year) VALUES( $1, $2) RETURNING *', [
+
+    artist = artistTable.rows[0];
+
+    const albumTable = await db.query('INSERT INTO Albums (name, year, artistid) VALUES( $1, $2, $3) RETURNING *', [
       'MM..FOOD',
       2004,
+      artist.id
     ])
-
-    album = rows[0]
+    album = albumTable.rows[0];
   })
 
   describe('DELETE /albums/{id}', () => {
     it('deletes the artist and returns the deleted data', async () => {
-      const { status, body } = await request(app).delete(`/albums/${album.id}`).send()
+      const { status, body } = await request(app)
+        .delete(`/albums/${album.id}`).send()
 
       expect(status).to.equal(200)
 
-      expect(body).to.deep.equal({ id: album.id, name: 'MM..FOOD', artistId: artist.id, year: 2004 })
+      expect(body).to.deep.equal({
+        id: album.id,
+        name: 'MM..FOOD',
+        year: 2004,
+        artistid: artist.id
+      })
     })
 
     it('returns a 404 if the album does not exist', async () => {
-      const { status, body } = await request(app).delete('/albums/999999999').send()
+      const { status, body } = await request(app)
+        .delete('/albums/999999999').send()
 
       expect(status).to.equal(404)
       expect(body.message).to.equal('album 999999999 does not exist')
--- tests/album-update.test.js
+++ tests/album-update.test.js
@@ -1,43 +1,49 @@
-const { expect } = require('chai');
-const request = require('supertest');
-const db = require('../src/db');
-const app = require('../src/app');
+const { expect } = require("chai");
+const request = require("supertest");
+const db = require("../src/db");
+const app = require("../src/app");
 
-describe('Update Album', () => {
-  let artist;
+describe("Update Album", () => {
   let album;
+  let artist;
   beforeEach(async () => {
-    const { rows } = await db.query('INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *', [
-      'MF DOOM',
-      'rap',
-    ]);
-    const { albumRows } = await db.query('INSERT INTO Albums (name, year, artistId) VALUES ($1, $2, $3) RETURNING *', [
-      'MM..FOOD',
-      2004,
-      4,
+    const artistTable = await db.query("INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *", [
+      "MF DOOM",
+      "rap"
     ]);
 
-    album = albumRows[0];
-    artist = rows[0];
+    artist = artistTable.rows[0];
+
+    const albumTable = await db.query("INSERT INTO Albums (name, year, artistId) VALUES ($1, $2, $3) RETURNING *", [
+      "MM..FOOD",
+      2004,
+      artist.id
+    ]);
+
+    album = albumTable.rows[0];
   });
 
-  describe('PATCH /albums/{id}', () => {
-    it('updates the album and returns the updated record', async () => {
+  describe("PATCH /albums/{id}", () => {
+    it("updates the album and returns the updated record", async () => {
       const { status, body } = await request(app)
         .patch(`/albums/${album.id}`)
-        .send({ name: 'Updated Album', year: 2022 });
-      console.log(body)
+        .send({ name: "Updated Album", year: 2022 });
       expect(status).to.equal(200);
-      expect(body).to.deep.equal({ id: album.id, name: 'Updated Album', year: 2022 });
+      expect(body).to.deep.equal({
+        id: album.id,
+        name: "Updated Album",
+        year: 2022,
+        artistid: artist.id,
+      });
     });
 
-    it('returns a 404 if the album does not exist', async () => {
+    it("returns a 404 if the album does not exist", async () => {
       const { status, body } = await request(app)
-        .patch('/albums/999999999')
-        .send({ name: 'MM..FOOD', year: 2004 });
+        .patch("/albums/999999999")
+        .send({ name: "MM..FOOD", year: 2004 });
 
       expect(status).to.equal(404);
-      expect(body.message).to.equal('album 999999999 does not exist');
+      expect(body.message).to.equal("album 999999999 does not exist");
     });
   });
 });
